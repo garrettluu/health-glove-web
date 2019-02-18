969af51d6d9d7d968fcfef1c851db1abb8937f10
Garrett Luu <garrettluu131@gmail.com>
2019-02-17 21:16:34 -0800

    Added socket.io functionality

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var socketio = require('socket.io');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var io = socketio();
+app.io = io;
 
 app.use(logger('dev'));
 app.use(express.json());
