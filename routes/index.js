3d8af4ff50b37ae458e98c44290eb546af1a6aef
Garrett Luu <garrettluu131@gmail.com>
2019-02-18 13:16:05 -0800

    Added socket.io receiver on server-side

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Receiving websockets */
+io.on('connection', function (socket) {
+    socket.on('update', function(msg) {
+      socket.emit('updateHeartRate', msg);
+    });
+});
+
 module.exports = router;
